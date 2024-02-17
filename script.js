777286790a26cd67a5008cd0acdd142e830ba513
Matthew Benedicta <matthewbenedicta2007@gmail.com>
2024-02-17 07:27:45 +0100

    write complete program

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ let input = parseInt(userInput);
 
 // console.log(input);
 
+if (!isNaN(input)) {
+  if (input % 2 === 0 && input % 3 === 0) {
+    console.log("Divisible by both 2 and 3");
+  } else if (input % 2 === 0) {
+    console.log("Divisible by 2");
+  } else if (input % 3 === 0) {
+    console.log("Divisible by 3");
+  } else {
+    console.log("Not divisible by both 2 and 3!");
+  }
+} else {
+  console.log("Not a number! Enter a number");
+}
